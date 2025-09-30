f80bc14be7cfe6b680522a796f061529c80f54f3
Darren Yilmaz <dyilmaz1@hwemail.com>
2025-09-30 01:27:01 -0700

    (GP-2.1): Program Can Initialize Repository

--- Git.java
+++ Git.java
@@ -1,5 +1,34 @@
+import java.io.IOException;
+import java.nio.file.*;
+
 public class Git {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        init();
+    }
+    
+    public static void init() throws IOException {
+        boolean didSomething = false;
+        if (!Files.exists(Path.of("git"))) {
+            Files.createDirectory(Path.of("git"));
+            didSomething = true;
+        }
+        if (!Files.exists(Path.of("git/objects"))) {
+            Files.createDirectory(Path.of("git/objects"));
+            didSomething = true;
+        }
+        if (!Files.exists(Path.of("git/index"))) {
+            Files.createFile(Path.of("git/index"));
+            didSomething = true;
+        }
+        if (!Files.exists(Path.of("git/HEAD"))) {
+            Files.createFile(Path.of("git/HEAD"));
+            didSomething = true;
+        }
+        if (didSomething) {
+            System.out.println("Git Repository Created");
+        }
+        else {
+            System.out.println("Git Repository Already Exists");
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # git-project-DARRENYILMAZ
 Git Project
+
+
+The init() method within the Git.java file initializes a git repository in the folder that the Git.java file is located in. Additionally, the main method of the Git.java file calls the init() method, meaning you can run that instead if you so choose. Challenges encountered include figuring out how to tell if a repository is already created.
